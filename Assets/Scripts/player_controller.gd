db2327855a9659e601490e9e33bb3d65f5b2d33d
Standby123 <feelysean15@gmail.com>
2026-06-19 19:30:01 +0100

    Added Player Script and World Floor
    Added a player script
    Added a script to Level 1, to handle the game loop
    Added a world boundary to the floor

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -1,3 +1,13 @@
 extends RigidBody2D
 
 var is_first_shot:bool = true
+
+var knockback_force : int = 1
+
+@onready var collider: CollisionPolygon2D = $Collider
+
+@onready var muzzle: Node2D = $Muzzle
+
+func impulse():
+	muzzle.get_angle_to(Vector2i.RIGHT)
+	#collider.Apply_impulse()
