c9a5298f9c0ccb07d34efb9063cdee371d80da86
Joe <joe.gibson9090@gmail.com>
2026-06-20 16:23:47 +0100

    Camera shake and cooldown
    - Implemented camera shake and made camera a scene that can be quickly imported to levels (though annoyingly the signal needs to be connected to the player each time a new level is created)
    - Created a level 2, its.. its not finished okay
    - Kept 0.4 second shot cooldown but changed implementation to now use godot's 'timer' node so this cooldown can scale with game speed if we wish :)

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -1,11 +1,8 @@
 extends RigidBody2D
 
-var is_first_shot:bool = true
-
 var recoil : int = 20
-var cooldown : float = 0.4 # cooldown time between shots in seconds
-var time_passed : float = 0 # time passed since last shot fired
-
+var shot_allowed : bool = true
+signal shot_fired
 
 @onready var collider: CollisionPolygon2D = $Collider
 
@@ -14,16 +11,11 @@ var time_passed : float = 0 # time passed since last shot fired
 func _physics_ready():
 	pass
 	
-
 func _physics_process(delta: float) -> void:
-	
-
-
-	
 	angular_damp = 2
 	
 	if abs(angular_velocity) > 10:
-		angular_velocity *= 0.9
+		angular_velocity *= 0.7 # joe slowed this from 0.9 to 0.7 :)
 	
 	if Input.is_action_pressed("Rotate Left"):
 		angular_velocity = -8
@@ -31,19 +23,17 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("Rotate Right"):
 		angular_velocity = 8
 		
-	# increase time passed
-	if time_passed < cooldown:
-		time_passed += delta
-	# if cooldown time has passed - allow a shot	
-	elif time_passed >= cooldown:
+	if shot_allowed == true:
 		if Input.is_action_just_pressed("Shoot"):
-			time_passed = 0
-			linear_velocity = Vector2(0,0)
+			shot_fired.emit() # emit a signal to say a shot was fired
+			shot_allowed = false
+			linear_velocity = Vector2(0,0) # set velocity to zero to allow snappy mid air turnarounds
 			var pos = muzzle.transform[2] # Get Muzzle Position
 			var dir = -1* Vector2(transform.x.x, transform.x.y) # Get direction to shoot
 			
 			# Physics Stuff
 			apply_impulse(dir * recoil, pos)
 
-
-	
+func _on_timer_timeout() -> void:
+	# allow another shot after cooldown times out
+	shot_allowed = true # Replace with function body.
