7cbfa3ea90e36f222e6efdee74a67b70d6ded475
Standby123 <feelysean15@gmail.com>
2026-06-20 11:36:28 +0100

    Rotational dampeners
    Added a rotational dampener
    Added a rotational limit
    Added an arrow so I an see what way anything is facing

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -2,17 +2,20 @@ extends RigidBody2D
 
 var is_first_shot:bool = true
 
-var knockback_force : int = 1
+var recoil : int = 20
 
 @onready var collider: CollisionPolygon2D = $Collider
 
 @onready var muzzle: Node2D = $Muzzle
 
+
 func _physics_process(delta: float) -> void:
+	
 	if Input.is_action_just_pressed("Shoot"):
-		var x_dir = muzzle.transform[0]
-		var y_dir = muzzle.transform[1]
 		var pos = muzzle.transform[2]
+		var dir = -1* Vector2(transform.x.x, transform.x.y)
 		
-		print(pos)
-		apply_impulse(Vector2i.UP * 10, pos)
+		apply_impulse(dir * recoil, pos)
+		angular_damp = 2
+		if abs(angular_velocity) > 8:
+			angular_velocity = 8
