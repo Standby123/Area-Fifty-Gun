e606bbc85adb866c60962740bb73fae69d2f575f
Standby123 <feelysean15@gmail.com>
2026-06-20 12:09:38 +0100

    Changed Character Controller
    Changed the Mass
    Removed Dampeners

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -8,14 +8,25 @@ var recoil : int = 20
 
 @onready var muzzle: Node2D = $Muzzle
 
+func _physics_ready():
+	pass
 
 func _physics_process(delta: float) -> void:
 	
-	if Input.is_action_just_pressed("Shoot"):
-		var pos = muzzle.transform[2]
-		var dir = -1* Vector2(transform.x.x, transform.x.y)
+	angular_damp = 2
+
+	if Input.is_action_pressed("Rotate Left"):
+		angular_velocity -= 5
 		
+		angular_velocity = 8
+		
+	if Input.is_action_just_pressed("Shoot"):
+		print(angular_velocity, angular_damp)	
+		var pos = muzzle.transform[2] # Get Muzzle Position
+		var dir = -1* Vector2(transform.x.x, transform.x.y) # Get direction to shoot
+		
+		# Physics Stuff
 		apply_impulse(dir * recoil, pos)
-		angular_damp = 2
-		if abs(angular_velocity) > 8:
-			angular_velocity = 8
+
+
+	
