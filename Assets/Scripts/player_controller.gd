4e466632182f7bb4b64fc2bd3fdfaabe5d15d193
Joe <joe.gibson9090@gmail.com>
2026-06-20 14:05:03 +0100

    Added shoot cooldown
    I modified the player controller script to add a 0.4 second (number might need tweaking) cooldown between shots fired to prevent wild spamming that bordered on teleporting lol

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -3,6 +3,9 @@ extends RigidBody2D
 var is_first_shot:bool = true
 
 var recoil : int = 20
+var cooldown : float = 0.4 # cooldown time between shots in seconds
+var time_passed : float = 0 # time passed since last shot fired
+
 
 @onready var collider: CollisionPolygon2D = $Collider
 
@@ -13,6 +16,9 @@ func _physics_ready():
 	
 
 func _physics_process(delta: float) -> void:
+	
+
+
 	
 	angular_damp = 2
 	
@@ -25,13 +31,19 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("Rotate Right"):
 		angular_velocity = 8
 		
-	if Input.is_action_just_pressed("Shoot"):
-		linear_velocity = Vector2(0,0)
-		var pos = muzzle.transform[2] # Get Muzzle Position
-		var dir = -1* Vector2(transform.x.x, transform.x.y) # Get direction to shoot
-		
-		# Physics Stuff
-		apply_impulse(dir * recoil, pos)
+	# increase time passed
+	if time_passed < cooldown:
+		time_passed += delta
+	# if cooldown time has passed - allow a shot	
+	elif time_passed >= cooldown:
+		if Input.is_action_just_pressed("Shoot"):
+			time_passed = 0
+			linear_velocity = Vector2(0,0)
+			var pos = muzzle.transform[2] # Get Muzzle Position
+			var dir = -1* Vector2(transform.x.x, transform.x.y) # Get direction to shoot
+			
+			# Physics Stuff
+			apply_impulse(dir * recoil, pos)
 
 
 	
