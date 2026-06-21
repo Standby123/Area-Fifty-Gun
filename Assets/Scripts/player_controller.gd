48b4d983485cdf55edbe9fb29110e0a4c51ff144
Joe <joe.gibson9090@gmail.com>
2026-06-21 13:49:14 +0100

    Remade shot cooldown and camera shake
    it got overwritten for some reason whoops

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -1,8 +1,8 @@
 extends RigidBody2D
 
-var is_first_shot:bool = true
-
+var shot_allowed : bool = true
 var recoil : int = 20
+signal shot_fired
 
 @onready var collider: CollisionPolygon2D = $Collider
 
@@ -17,18 +17,25 @@ func _physics_process(delta: float) -> void:
 	angular_damp = 2
 	
 	if abs(angular_velocity) > 10:
-		angular_velocity *= 0.9
+		angular_velocity *= 0.7
 	
 	if Input.is_action_pressed("Rotate Left"):
 		angular_velocity = -8
 		
 	if Input.is_action_pressed("Rotate Right"):
 		angular_velocity = 8
-		
-	if Input.is_action_just_pressed("Shoot"):
-		linear_velocity = Vector2(0,0)
-		var pos = muzzle.transform[2] # Get Muzzle Position
-		var dir = -1* Vector2(transform.x.x, transform.x.y) # Get direction to shoot
-		
-		# Physics Stuff
-		apply_impulse(dir * recoil, pos)
+	if shot_allowed == true:	
+		if Input.is_action_just_pressed("Shoot"):
+			shot_allowed = false
+			shot_fired.emit() # emit a signal to say a shot was fired
+			linear_velocity = Vector2(0,0)
+			var pos = muzzle.transform[2] # Get Muzzle Position
+			var dir = -1* Vector2(transform.x.x, transform.x.y) # Get direction to shoot
+			
+			# Physics Stuff
+			apply_impulse(dir * recoil, pos)
+
+
+func _on_timer_timeout() -> void:
+	# allow another shot after cooldown times out
+	shot_allowed = true # Replace with function body.
