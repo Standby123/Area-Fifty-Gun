8df09b20b0aef9aac176870df368d6a3c337a8c1
Standby123 <feelysean15@gmail.com>
2026-06-21 13:41:12 +0100

    Level Changer Completed
    Added a Level Changer node and script.
    Updates to the next sequential level

--- Assets/Scripts/level_end_area.gd
+++ Assets/Scripts/level_end_area.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-
 func _on_body_entered(body: Node2D) -> void:
 	body.queue_free() # Remove player from scene
-	
-	get_tree().change_scene_to_file('res://Assets/Scenes/Levels/level2.tscn')
+	var current = get_tree().current_scene.scene_file_path
+	var next_scene = "res://Assets/Scenes/Levels/level" + str(current.to_int()+1) + ".tscn"
+	get_tree().change_scene_to_file.call_deferred(next_scene)
