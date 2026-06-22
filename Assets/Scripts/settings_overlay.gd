c513cc58b44ab2b1cdc7823681877ebe78947c0f
Standby123 <feelysean15@gmail.com>
2026-06-22 21:14:31 +0100

    Added Reset Level and Restart Game

--- Assets/Scripts/settings_overlay.gd
+++ Assets/Scripts/settings_overlay.gd
@@ -18,3 +18,17 @@ func _input(event):
 			animation_player.play("Write Text")
 			get_tree().paused = true
 			
+
+
+
+func _on_level_reset_pressed() -> void:
+	get_tree().change_scene_to_file.call_deferred(get_tree().current_scene.scene_file_path)
+	visible = false
+	animation_player.stop()
+	get_tree().paused = false
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file.call_deferred("res://Assets/Scenes/Levels/level1.tscn")
+	visible = false
+	animation_player.stop()
+	get_tree().paused = false
