31f6fd2d429be1d04c2d8184748571110f8df9ad
Standby123 <feelysean15@gmail.com>
2026-06-25 19:43:15 +0100

    Added Music

--- Assets/Scripts/main_menu.gd
+++ Assets/Scripts/main_menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	MusicPlayer.play_certain_track(1)
+
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Assets/Scenes/Levels/level1.tscn")
 
---
+++ Assets/Scripts/music_player.gd
@@ -0,0 +1,17 @@
+extends AudioStreamPlayer2D
+
+const game = preload("res://Assets/Audio/Music/Game music.mp3")
+const menu = preload("res://Assets/Audio/Music/very serious2.mp3")
+
+func play_certain_track(track_number: int):
+	# Stop the player to prevent overlapping audio
+	stop()
+	
+	# Assign the specific track
+	if track_number == 1:
+		stream = menu
+	elif track_number == 2:
+		stream = game
+		
+	# Start playing the newly assigned track
+	play()
